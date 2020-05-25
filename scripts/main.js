88e19e0c6ad3ee7a46110ad1d537109f32859979
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-05-25 16:00:12 +0530

    Add book object

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    
+}
