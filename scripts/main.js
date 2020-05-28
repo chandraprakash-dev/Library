f1f752b6f444914e1caf0d1723fcf57962cf384d
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-05-28 12:30:52 +0530

    Add books to library array

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,29 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
-    
+function displayBooks() {
+    console.log(myLibrary);
 }
 
+function addBookToLibrary() {
+    let title = document.info.Title.value;
+    let author = document.info.Author.value;
+    let pages = document.info.Pages.value;
+    let read = document.info.Read.value;
+
+    let tmp = new Book(title, author, pages, read);
+    myLibrary.push(tmp);
+}
 
 // const form = document.createElement('form');
 // const input = document.createElement('input');
 // form.appendChild(input);
 // const main = document.querySelector('main');
 // main.appendChild(form);
+
+const submit = document.querySelector(`input[id="new"]`);
+submit.addEventListener('click', addBookToLibrary);
+
+const display = document.querySelector(`button[name="books"]`);
+display.addEventListener('click', displayBooks);
 
